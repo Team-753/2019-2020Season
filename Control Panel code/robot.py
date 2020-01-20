10f3d65edb4fd88d1e5248d93c841ec30b6e3dd6
Scouting753 <team753code@gmail.com>
2020-01-20 15:43:54 -0800

    started fixing the bb8 code

--- Control Panel code/robot.py
+++ Control Panel code/robot.py
@@ -8,11 +8,16 @@ class MyRobot(wpilib.TimedRobot):
 	
 	def robotInit(self):
 	
-		self.green =1
-		self.red= 1
-		self.blue =1
-		self.yellow= 1
-		self.spinButton= self.ds
+		self.green = 1
+		self.red = 1
+		self.blue = 1
+		self.yellow = 1
+		# these buttons are for the different stages, where the wheel will spin until it has the right color or the right number of rotations
+		self.rotationsSpinButton = wpilib.DriverStation.getStickButton(0,0) #joystick channel and button number
+		self.colorSpinButton = wpilib.DriverStation.getStickButton(0,1)
+		self.ethansMotor = rev.CANSparkMax(4,rev.MotorType.kBrushless)
+		self.ethansServo = wpilib.PWM(1) #this is the servo that flips up the wheel, it is connected to pwm channel 1
+		# .setPosition(0.0-1.0) means -90 degrees to 90 degrees
 
 		self.spinning = False
 		self.targetColor= green
@@ -25,17 +30,16 @@ class MyRobot(wpilib.TimedRobot):
 	
 
 
-
 		
 	def teleopInit(self):
 		pass
 	def teleopPeriodic(self):
 		#use this to control the motor: rev.CANSparkMax(4,rev.MotorType.kBrushless).set(0.5)
-		if (self.spinButton== True) or self.spinning:
+		if (self.spinButton == True) or self.spinning:
 		#rev.MotorType.kBrushless
 		#rev.CANSparkMax(4,rev.MotorType.kBrushless).getEncoder()
 			if colorChanges < 21:
-				rev.CANSparkMax(4,rev.MotorType.kBrushless).set(0.3)
+				self.ethansMotor.set(0.3)
 				self.spinning = True
 				
 			if self.colorChanges > 21:
@@ -43,14 +47,14 @@ class MyRobot(wpilib.TimedRobot):
 		
 		#colors go green,blue yellow red green
 		#positive is clockwise and negative is counterclockwise
-		if self.colorButton== True:
+		if self.colorButton == True:
 			if self.targetColor == green and self.colorSensor == blue:
 				self.revTarget = 2
 			
 			if self.targetColor == blue and self.colorSensor == green:
-				self.revTarget= 2
+				self.revTarget = 2
 			
-			if self.targetColor== yellow and self.colorSensor == green:
+			if self.targetColor == yellow and self.colorSensor == green:
 				self.revTarget = 0
 			if
 		
