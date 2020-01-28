04624dfcf3d7153f4d41a2c8b7fa54986687b758
Scouting753 <team753code@gmail.com>
2020-01-27 17:50:38 -0800

    I love purging!!!
    The vile infidel nonsense (still possibly more organized than anything we have done) has been purged. Our code is now receptive to the glorious revelations of our collective intellect.

--- Full Swerve/Java Swerve/Swerve main/src/main/java/org/frcteam2910/c2019/Robot.java
+++ Full Swerve/Java Swerve/Swerve main/src/main/java/org/frcteam2910/c2019/Robot.java
@@ -4,36 +4,32 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.frcteam2910.c2019.autonomous.AutonomousSelector;
-import org.frcteam2910.c2019.autonomous.AutonomousTrajectories;
-import org.frcteam2910.c2019.subsystems.*;
-import org.frcteam2910.c2019.vision.api.Gamepiece;
-import org.frcteam2910.common.robot.drivers.Limelight;
-import org.frcteam2910.common.robot.drivers.NavX;
-import org.frcteam2910.common.robot.subsystems.SubsystemManager;
+import org.frcteam753.c2020.autonomous.AutonomousSelector;
+import org.frcteam753.c2020.autonomous.AutonomousTrajectories;
+import org.frcteam753.c2020.subsystems.*;
+import org.frcteam753.c2020.vision.api.Gamepiece;
+import org.frcteam753.common.robot.drivers.Limelight;
+import org.frcteam753.common.robot.drivers.NavX;
+import org.frcteam753.common.robot.subsystems.SubsystemManager;
 
 public class Robot extends TimedRobot {
     private static final double UPDATE_DT = 5e-3; // 5 ms
 
     private final SubsystemManager subsystemManager = new SubsystemManager(
-            ClimberSubsystem.getInstance(),
+            
             DrivetrainSubsystem.getInstance(),
-            CargoGrabberSubsystem.getInstance(),
-            CargoArmSubsystem.getInstance(),
-            HatchFloorGathererSubsystem.getInstance(),
-            HatchPlacerSubsystem.getInstance(),
-            VisionSubsystem.getInstance()
+            
     );
 
     private static final OI oi = new OI();
 
-    private AutonomousTrajectories autonomousTrajectories = new AutonomousTrajectories(DrivetrainSubsystem.CONSTRAINTS);
+    
     private AutonomousSelector autonomousSelector = new AutonomousSelector(autonomousTrajectories);
 
     private Command autonomousCommand = null;
 
     public Robot() {
-        oi.bindButtons(autonomousSelector);
+       
     }
 
     public static OI getOi() {
@@ -42,25 +38,13 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotInit() {
-        SmartDashboard.putBoolean("Limelight Calibration Mode", false);
-
+        
         subsystemManager.enableKinematicLoop(UPDATE_DT);
     }
 
     @Override
     public void robotPeriodic() {
-        subsystemManager.outputToSmartDashboard();
-
-        SmartDashboard.putNumber("Arm Angle",
-                Math.toDegrees(CargoArmSubsystem.getInstance().getCurrentAngle()));
-        SmartDashboard.putNumber("Robot Angle",
-                Superstructure.getInstance().getGyroscope().getAngle().toDegrees());
-        SmartDashboard.putNumber("Gyro Pitch",
-                Math.toDegrees(Superstructure.getInstance().getGyroscope().getAxis(NavX.Axis.ROLL)));
-        SmartDashboard.putBoolean("Is Competition Bot",
-                Superstructure.getInstance().isCompetitionBot());
-        SmartDashboard.putBoolean("Is Practice Bot",
-                Superstructure.getInstance().isPracticeBot());
+        
     }
 
     @Override
