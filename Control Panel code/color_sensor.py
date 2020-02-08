a58d9e969c779d81dc09fface6d5d92ed574fd6e
Scouting753 <team753code@gmail.com>
2020-02-07 19:31:47 -0800

    Update color_sensor.py

--- Control Panel code/color_sensor.py
+++ Control Panel code/color_sensor.py
@@ -5,48 +5,48 @@ from rev.color import ColorSensorV3
 
 class ColorSensor(self):
 	def __init__(self):
-		colorSensor = ColorSensorV3(wpilib.I2C.Port.kOnboard)
-		predictedColor = ""
-		rotations = 0
-		lastColorIndex = 0
-		colorOrder = ["yellow","red","green","cyan"]
+		self.colorSensor = ColorSensorV3(wpilib.I2C.Port.kOnboard)
+		self.predictedColor = ""
+		self.rotations = 0
+		self.lastColorIndex = 0
+		self.colorOrder = ["yellow","red","green","cyan"]
 	
 	def predictColor(self):
-		red = colorSensor.getColor().red
-		green = colorSensor.getColor().green
-		blue = colorSensor.getColor().blue
+		red = self.colorSensor.getColor().red
+		green = self.colorSensor.getColor().green
+		blue = self.colorSensor.getColor().blue
 		
 		if 0.29 < red < 0.53 and 0.33 < green < 0.46 and 0.13 < blue < 0.26:
-			predictedColor = "red"
+			self.predictedColor = "red"
 		elif 0.26 < red < 0.35 and 0.49 < green < 0.57 and 0.12 < blue < 0.24:
-			predictedColor = "yellow"
+			self.predictedColor = "yellow"
 		elif 0.16 < red < 0.25 and 0.48 < green < 0.58 and 0.24 < blue < 0.28:
-			predictedColor = "green"
+			self.predictedColor = "green"
 		elif 0.11 < red < 0.23 and 0.41 < green < 0.47 and 0.31 < blue < 0.47:
-			predictedColor = "cyan"
+			self.predictedColor = "cyan"
 		else:
-			predictedColor = "???"
+			self.predictedColor = "???"
 		
-		return(predictedColor)
+		return(self.predictedColor)
 		
 	def startRotationCount(self):
 		
-		if lastColorIndex <= 2:
-			if predictedColor == colorOrder[lastColorIndex + 1]:
-				rotations += 0.125
-				lastColorIndex += 1
-		elif lastColorIndex == 3:
-			if predictedColor == colorOrder[lastColorIndex - 3]:
-				rotations += 0.125
-				lastColorIndex -= 3
+		if self.lastColorIndex <= 2:
+			if self.predictedColor == self.colorOrder[self.lastColorIndex + 1]:
+				self.rotations += 0.125
+				self.lastColorIndex += 1
+		elif self.lastColorIndex == 3:
+			if self.predictedColor == self.colorOrder[self.lastColorIndex - 3]:
+				self.rotations += 0.125
+				self.lastColorIndex -= 3
 		
 	def checkRotationCount(self):
 		
-		return(rotations)
+		return(self.rotations)
 		
 	def resetRotationCount(self):
 		
-		rotations = 0
+		self.rotations = 0
 		
 if __name__ == "__main__":
 	wpilib.run(colorSensor)
