80ff15fc0ea9dcd31a4790f2fb02c114e5ed6bd0
LiamCrafton <ldcrafty@gmail.com>
2020-02-11 22:36:03 -0800

    2/11 Code Revision
    Fixed slight syntax, added an encoder/basic encoder functions to the climb class, and changed ManualTurret name as eventually that will include more than just manual control

--- Swerve to Appease Liam/Climb.py
+++ Swerve to Appease Liam/Climb.py
@@ -2,9 +2,12 @@ import wpilib
 import rev
 
 class Climb:
+	encoderConversionFactor = 64 #this isn't real and needs to be changed
 	def __init__(self, motorID, speed):
-		self.climbMotor = rev.CANSparkMax(ID, MotorType.kBrushless)
+		self.climbMotor = rev.CANSparkMax(ID, rev.MotorType.kBrushless)
 		self.speed = speed
+		self.climbEncoder = self.climbMotor.getEncoder()
+		self.climbEncoder.setPositionConversionFactor(self.encoderConversionFactor)
 	def extend(self):
 		self.climbMotor.set(self.speed)
 	def contract(self):
@@ -13,3 +16,8 @@ class Climb:
 		self.climbMotor.setIdleMode(rev.IdleMode.kCoast)
 	def brake(self):
 		self.climbMotor.setIdleMode(rev.IdleMode.kBrake)
+	def checkEncoder(self):
+		position = self.climbEncoder.getPosition()
+		return position
+	def zeroEncoder(self):
+		self.climbEncoder.setPosition(0)
--- Swerve to Appease Liam/Turret.py
+++ Swerve to Appease Liam/Turret.py
@@ -1,8 +1,8 @@
 import wpilib
 import rev
 
-class ManualTurret:
-	def __init__(self, rotateID, flyWheelID, speed):
+class Turret: #this is currently just manual control, but motors can only be instantiated once so
+	def __init__(self, rotateID, flyWheelID, speed): #eventually all the turret stuff will go here
 		self.rotateMotor = rev.CANSparkMax(rotateID, MotorType.kBrushless)
 		self.rotateEncoder = self.rotateMotor.getEncoder()
 		self.flyWheelMotor = rev.CANSparkMax(flyWheelID, MotorType.kBrushless)
